e8a829b0459099c8c31e5e5f339cdbdbd353fc04
Rimon Moomey <rimon.moomey@gmail.com>
2022-10-31 17:21:21 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(array) {
+    return array.slice(0, 2);
+}
+
+function returnLastTwoDrivers(array) {
+    return array.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return (function(factor) {
+        return multiplier * factor;
+    })
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, callback) {
+    return callback(arrayOfDrivers);
+}
